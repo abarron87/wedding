b0526778851b27c4b09e6a186db38a6905099c16
Alex Barron <alexbarron.barron@gmail.com>
2016-01-31 19:38:34 +0100

    Added latest copy. Added wow.js to animate page sections.

---
+++ app/js/main.js
@@ -0,0 +1,31 @@
+// Init Wow.js for animating in page content.
+(function(){
+    new WOW().init();
+})();
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    //var html
+    //var scrollTop =
+    var nav = $('.navbar'),
+        navOffset = nav.offset().top,
+        fixedClass = 'navbar-fixed-top';
+
+    $(window).scroll(function(event) {
+        var $elem = $(this);
+
+        if($elem.scrollTop() >= navOffset){
+            if(!nav.hasClass(fixedClass)) {
+                nav.addClass(fixedClass);
+            }
+        }
+        else{
+            nav.removeClass(fixedClass);
+        }
+
+        //$('html, body').stop().animate({
+        //    scrollTop: $($anchor.attr('href')).offset().top
+        //}, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
